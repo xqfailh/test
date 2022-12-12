7e023864a7e9c8f1a4d8f5ef052036f9885f8e5f
xqfailh <44352637+xqfailh@users.noreply.github.com>
2022-12-12 18:28:01 +0800

    Add files via upload

---
+++ md5.php
@@ -0,0 +1,20 @@
+<?php
+include("./fffffffflag.php");
+error_reporting(0);
+if (isset($_GET['a']) and isset($_GET['b'])) {
+	if ($_GET['a'] == $_GET['b']) {
+		die('a can\'t be b');
+	}
+	elseif (md5($_GET['a']) === md5($_GET['b'])) {
+		die('Flag:'.$flag.'
+		Pri:'.$Pri);
+	}
+	elseif (md5($_GET['a']) == md5($_GET['b'])) {
+		die('Pri:'.$Pri);
+	}
+}
+else
+{
+	print '无参数';
+}
+?>
